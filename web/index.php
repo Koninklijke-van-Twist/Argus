dae49533762a07b7b8c6ef87d560dc9faeb11941
Tim Falken <tfalken@kvt.nl>
2026-03-18 16:17:05 +0100

    auth include

--- web/index.php
+++ web/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+require_once __DIR__ . 'auth.php';
 require_once __DIR__ . '/logincheck.php';
 
 $query = trim((string) ($_SERVER['QUERY_STRING'] ?? ''));
